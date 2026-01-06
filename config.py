77fd3e5d3cc2fa778f53070718f2dffbe526d274
novanverif <novan.verif@imel.app>
2026-01-06 19:01:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram Bot 配置
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8589991283:AAHax-5xTEkuGq8YwECm7Yg3q0JDLkU6mcg")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "pk_oa")
-CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/pk_oa")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7956048066:AAHbb6kYME7ycsnn0XdCM0Y9-ksakc76dEQ")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "tesbot_verif")
+CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/tesbot_verif")
 
 # 管理员配置
-ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "1617374407"))
+ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "5957445951"))
 
 # 积分配置
 VERIFY_COST = 1  # 验证消耗的积分
